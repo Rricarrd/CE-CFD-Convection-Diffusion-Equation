cf07cd312874642f23f9cc67ed2d0f62033b04e6
Rricarrd <rricarrd@gmail.com>
2024-10-19 10:57:21 +0200

    Schemes + plotting

---
+++ plot_phi_outlet.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = plot_phi_outlet(inputArg1,inputArg2)
+%PLOT_PHI_OUTLET Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
